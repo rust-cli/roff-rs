c300e9816d2b70c0906bdf9486db81fd2c5d9b4b
Sondre Nilsen <sondre@eons.io>
2021-12-17 14:53:32 +0100

    chore: Create example

---
+++ examples/demo.rs
@@ -0,0 +1,59 @@
+extern crate roff;
+
+// View this example by running `cargo run --example demo | man -l -`.
+
+fn main() {
+    use roff::*;
+
+    let page = Roff::new("corrupt", 1)
+        .date("2021-12-25")
+        .manual("General Commands Manual")
+        .source("corrupt v1")
+        .section(
+            "name",
+            &["corrupt - modify files by randomly changing bits"],
+        )
+        .section(
+            "SYNOPSIS",
+            &[
+                bold("corrupt"),
+                " ".into(),
+                "[".into(),
+                bold("-n"),
+                " ".into(),
+                italic("BITS"),
+                "]".into(),
+                " ".into(),
+                "[".into(),
+                bold("--bits"),
+                " ".into(),
+                italic("BITS"),
+                "]".into(),
+                " ".into(),
+                italic("file"),
+                "...".into(),
+            ],
+        )
+        .section(
+            "description",
+            &[
+                bold("corrupt"),
+                " modifies files by toggling a randomly chosen bit.".into(),
+            ],
+        )
+        .section(
+            "options",
+            &[list(
+                &[
+                    bold("-n"),
+                    ", ".into(),
+                    bold("--bits"),
+                    "=".into(),
+                    italic("BITS"),
+                ],
+                &["Set the number of bits to modify. ", "Default is one bit."],
+            )],
+        );
+
+    println!("{}", page.render());
+}
